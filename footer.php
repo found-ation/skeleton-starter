db50207afb2b717d8b83c19a3e3177e5f612b40e
Lee Miller <articlewiki@icloud.com>
2020-06-24 17:52:35 +0100

    Added Docs
    Added the new documents post type to the footer.

--- footer.php
+++ footer.php
@@ -43,7 +43,7 @@
                                         </ul>
                                     </div>
                                     <div class="col-lg-3 col-md-6 mb-5 mb-md-0">
-                                        <div class="text-uppercase-expanded text-xs mb-4 text-white lead">News</div>
+                                        <div class="text-uppercase-expanded text-xs mb-4 text-white lead">Docs</div>
                                         <ul class="list-unstyled mb-0">
                                             <?php 
                                             $homepageEvents = new WP_Query(array(
@@ -62,7 +62,7 @@
                                             <?php 
                                             $homepageEvents = new WP_Query(array(
                                             'posts_per_page' => 8,
-                                            'post_type' => 'page'
+                                            'post_type' => 'document'
                                             ));
                                             while($homepageEvents->have_posts()) {
                                             $homepageEvents->the_post(); ?>                                          
